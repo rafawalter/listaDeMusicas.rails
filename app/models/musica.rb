cc208eacfe3b9a2a6e47a98a1a1811551cdf4e9f
Rafael Walter <rafawalter@gmail.com>
2018-02-11 18:45:01 -0200

    validação dos campos

--- app/models/musica.rb
+++ app/models/musica.rb
@@ -1,2 +1,5 @@
 class Musica < ApplicationRecord
+  validates :nome, :temas, :cifras, presence: true
+  validates :nome, uniqueness: true  
+  validates :popularidade, numericality: { greater_than_or_equal_to: 0.0 }, allow_blank: true
 end
