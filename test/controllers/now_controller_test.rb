227895cf569391331f26b9e1fe8adde188fcfba7
Rafael Walter <rafawalter@gmail.com>
2018-02-21 07:42:16 -0300

    teste do controller

--- test/controllers/now_controller_test.rb
+++ test/controllers/now_controller_test.rb
@@ -4,6 +4,11 @@ class NowControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get now_index_url
     assert_response :success
+
+    assert_select 'nav.side_nav a', minimun: 4
+    assert_select 'main ul.musicas li', 3
+    assert_select 'h2', 'Fogo Suave'
+    assert_select '.temas', /\w+/
   end
 
 end
